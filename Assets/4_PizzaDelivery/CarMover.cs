feb65858553aabc802ba9b09d5cdaff0ac25f231
Yee <yee.gamedev@gmail.com>
2024-02-16 11:47:23 -0600

    Car Basic Animations Created

--- Assets/4_PizzaDelivery/CarMover.cs
+++ Assets/4_PizzaDelivery/CarMover.cs
@@ -8,10 +8,12 @@ namespace PZD.Movement
         [SerializeField] float turnSpeed = 4f;
 
         Rigidbody rb;
+        Animator animator;
 
         private void Awake()
         {
             rb = GetComponent<Rigidbody>();
+            animator = GetComponent<Animator>();
         }
 
         public void Move(float zAxis)
@@ -19,6 +21,8 @@ namespace PZD.Movement
             Vector3 directionalSpeed = transform.forward.normalized * zAxis * speed;
             directionalSpeed.y = rb.velocity.y;
             rb.velocity = directionalSpeed;
+
+            animator.SetFloat("zAxis", zAxis);
         }
 
         public void Turn(float xAxis)
