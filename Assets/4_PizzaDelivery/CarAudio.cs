216bb765509fcfb10fe79d8dc22f5bb919d2ea74
Yee <yee.gamedev@gmail.com>
2024-02-16 12:27:40 -0600

    Car Audio Script Written

---
+++ Assets/4_PizzaDelivery/CarAudio.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PZD.Audio
+{
+    public class CarAudio : MonoBehaviour
+    {
+        [SerializeField] [Range(0.8f, 1.1f)] float minPitch = 0.9f;
+        [SerializeField] [Range(0.9f, 1.3f)]float maxPitch = 1.2f;
+        [SerializeField] [Range(0, 1)] float pitchChangeRate = 0.5f;
+
+        AudioSource audioSource;
+
+        private void Awake() => audioSource = GetComponent<AudioSource>();
+
+        public void EngineSound(float speed)
+        {
+            audioSource.pitch += speed * pitchChangeRate;
+            audioSource.pitch = Mathf.Clamp(audioSource.pitch, minPitch, maxPitch);
+        }
+    }
+}
