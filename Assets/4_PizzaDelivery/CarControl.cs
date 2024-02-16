c5889c7ec25628c65ba3e85047f9ae59ded49a6c
Yee <yee.gamedev@gmail.com>
2024-02-16 10:25:04 -0600

    Car Moves

---
+++ Assets/4_PizzaDelivery/CarControl.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace PZ.Controls
+{
+    public class CarControl : MonoBehaviour
+    {
+        [SerializeField] float speed;
+        [SerializeField] float turnSpeed;
+
+        Rigidbody rb;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetButton("Vertical"))
+            { transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Horizontal") * turnSpeed, 0); }
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 directionalSpeed = transform.forward.normalized * Input.GetAxis("Vertical") * speed;
+            rb.velocity = directionalSpeed;
+        }
+    }
+}
