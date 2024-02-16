02450b9d157a4a75aba9dcf3537de0b450871d54
Yee <yee.gamedev@gmail.com>
2024-02-15 23:33:40 -0600

    Folder Organization

---
+++ Assets/2_0_ConditionalFSM/Scripts/ConditionalFSM.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CFSM
+{
+    public class ConditionalFSM : MonoBehaviour
+    {
+        [SerializeField] ConditionalState initialState;
+        [SerializeField] Text stateText;
+
+        const string stateMessage = "Estado actual: ";
+
+        MeshFilter meshFilter;
+        ConditionalState currentState;
+
+        private void Awake()
+        {
+            meshFilter = GetComponent<MeshFilter>();
+
+            currentState = initialState;
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Horizontal"))
+            {
+                currentState = currentState.GetState((int)Input.GetAxisRaw("Horizontal"));
+                meshFilter.mesh = currentState.Mesh;
+                stateText.text = stateMessage + currentState.transform.name;
+            }
+        }
+    }
+}
---
+++ Assets/2_0_ConditionalFSM/Scripts/ConditionalState.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace CFSM
+{
+    public class ConditionalState : MonoBehaviour
+    {
+        [SerializeField] ConditionalState forwardState;
+        [SerializeField] ConditionalState backwardState;
+        [SerializeField] Mesh mesh;
+
+        public Mesh Mesh => mesh;
+
+        public ConditionalState GetState(int condition) => condition > 0 ? forwardState : backwardState;
+    }
+}
